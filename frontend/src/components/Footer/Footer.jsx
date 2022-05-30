887abd32f78a65fd07b8ff63c2f97cb0aeafcf27
Aniket Singh <31aniket.singh@gmail.com>
2022-05-30 20:17:16 +0530

    dynamic date added

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,10 +2,11 @@ import "./Footer.scss";
 import logo from "../../assets/logo.png";
 
 const Footer = () => {
+  let currentYear = new Date().getFullYear();
   return (
     <div className="footer">
       <img src={logo} style={{ width: "100px", margin: "0 10px" }} />
-      <p className="footerfont">2021 All rights reserved</p>
+      <p className="footerfont">{currentYear} All rights reserved</p>
     </div>
   );
 };
